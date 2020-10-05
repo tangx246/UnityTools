c044db8ec6ccadb3eff7eab214a7f9ce810f0408
Yee Chien Tang <tangx246@umn.edu>
2020-10-05 11:22:01 -0700

    Added AudioClips

---
+++ Assets/Scripts/Sound/AudioClips.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace UnityTools
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class AudioClips : MonoBehaviour
+    {
+        [SerializeField] private AudioSource audioSource;
+        public AudioClip[] audioClips;
+
+        // Start is called before the first frame update
+        void OnValidate()
+        {
+            audioSource = GetComponent<AudioSource>();
+        }
+
+        public void PlayClip(int clip)
+        {
+            audioSource.PlayOneShot(audioClips[clip]);
+        }
+    }
+}
