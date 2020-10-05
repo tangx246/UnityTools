b851bc076d4a0be16bf9e9453161ba84038c91ab
Yee Chien Tang <tangx246@umn.edu>
2020-10-05 11:00:44 -0700

    Added Resource scripts

---
+++ Assets/Scripts/Resource/DrainingResource.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace UnityTools
+{
+    public class DrainingResource : Resource<float>
+    {
+        public float resourceDrainRate = 0.0033f; // Have to eat every 5 minutes
+        public float resourceTickRateSeconds = 1f;
+
+        private Coroutine coroutine;
+        public UnityEvent resourceOut = new UnityEvent();
+        public UnityEvent resourceFilled = new UnityEvent();
+
+        private void OnEnable()
+        {
+            coroutine = StartCoroutine(DrainResource());
+        }
+
+        private void OnDisable()
+        {
+            StopCoroutine(coroutine);
+        }
+
+        public IEnumerator DrainResource()
+        {
+            while (true)
+            {
+                value -= resourceDrainRate;
+                value = Math.Max(0, value);
+
+                if (value <= 0)
+                {
+                    resourceOut.Invoke();
+                }
+                else if (value >= 1)
+                {
+                    resourceFilled.Invoke();
+                }
+                yield return new WaitForSeconds(resourceTickRateSeconds);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Resource/Resource.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace UnityTools
+{
+    public class Resource<T> : MonoBehaviour where T : IComparable
+    {
+        public T maxValue;
+
+        [SerializeField]
+        private T _value;
+        public T value
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                if (_value.CompareTo(maxValue) > 0)
+                {
+                    _value = maxValue;
+                }
+                else
+                {
+                    _value = value;
+                }
+
+                resourceUpdated.Invoke(_value);
+            }
+        }
+        public ResourceUpdated resourceUpdated = new ResourceUpdated();
+
+        [Serializable]
+        public class ResourceUpdated : UnityEvent<T> { }
+
+        public virtual void Start()
+        {
+            resourceUpdated.Invoke(value);
+        }
+    }
+}
