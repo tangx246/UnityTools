7d248ca70455ceb59c0ea2d82986daacddd31464
Yee Chien Tang <tangx246@umn.edu>
2020-10-16 10:50:20 -0700

    Added EventVar

---
+++ Assets/Scripts/Events/EventVar.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine.Events;
+
+namespace UnityTools
+{
+    public class EventVar<T>
+    {
+        public readonly UnityEvent<T> valueChangedEvent = new UnityEvent<T>();
+        private T value;
+
+        public void Set(T value)
+        {
+            this.value = value;
+            valueChangedEvent.Invoke(value);
+        }
+
+        public T Get()
+        {
+            return value;
+        }
+    }
+}
