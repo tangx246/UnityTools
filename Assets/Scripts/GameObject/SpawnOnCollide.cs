dcfd40ffe5bfa3c429a06611b3528087fcf46b37
Yee Chien Tang <tangx246@umn.edu>
2020-11-04 15:10:12 -0800

    Added SpawnOnCollide

---
+++ Assets/Scripts/GameObject/SpawnOnCollide.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class SpawnOnCollide : MonoBehaviour
+{
+    public GameObject prefabToSpawn;
+
+    public void OnCollisionEnter()
+    {
+        Instantiate(prefabToSpawn, transform.position, transform.rotation);
+    }
+}
