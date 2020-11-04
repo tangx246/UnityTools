6a372556cbdbc6291337451a4044e1ebed340113
Yee Chien Tang <tangx246@umn.edu>
2020-11-04 15:00:11 -0800

    Added TimedDestroy script

---
+++ Assets/Scripts/GameObject/TimedDestroy.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace UnityTools
+{
+    public class TimedDestroy : MonoBehaviour
+    {
+        public float maxLifeSeconds = 3f;
+
+        void Start()
+        {
+            Destroy(gameObject, maxLifeSeconds);
+        }
+    }
+}
