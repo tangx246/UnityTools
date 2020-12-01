69681aeda5f7bd0e0e464e211572af1cca0e6ed5
Yee Chien Tang <tangx246@umn.edu>
2020-12-01 14:03:05 -0800

    Added External folder and moved SerializableDictionary and PandaBehavior as external dependencies. Added some basic NavMeshAgent-AI behaviors

---
+++ Assets/Scripts/AI/NavMeshAgent/RandomWandering.cs
@@ -0,0 +1,65 @@
+﻿using Panda;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class RandomWandering : MonoBehaviour
+{
+    public float wanderIntervalSeconds = 1f;
+    public float wanderRadius = 5f;
+
+    [SerializeField] private NavMeshAgent navMeshAgent;
+    private float idleStartTime;
+
+    private void OnValidate()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    [Task]
+    public void WanderToRandomPosition()
+    {
+        if (Task.current.isStarting)
+        {
+            Vector3 randomDirection = Random.insideUnitSphere * wanderRadius;
+            randomDirection += transform.position;
+
+            NavMeshHit hit;
+            NavMesh.SamplePosition(randomDirection, out hit, wanderRadius, 1);
+
+            navMeshAgent.destination = hit.position;
+
+            return;
+        }
+
+        if (navMeshAgent.pathPending)
+        {
+            return;
+        }
+
+        if (navMeshAgent.remainingDistance == Mathf.Infinity)
+        {
+            Task.current.Fail();
+        }
+
+        if (navMeshAgent.remainingDistance <= 0)
+        {
+            Task.current.Succeed();
+        }
+    }
+
+    [Task]
+    public void Idle()
+    {
+        if (Task.current.isStarting)
+        {
+            idleStartTime = Time.time;
+            return;
+        }
+
+        if (Time.time - idleStartTime < wanderIntervalSeconds)
+        {
+            Task.current.Succeed();
+        }
+    }
+}
