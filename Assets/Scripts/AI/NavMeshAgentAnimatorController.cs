9503d489b64b11687ed60a3366031c0ab25034da
Yee Chien Tang <tangx246@umn.edu>
2020-12-02 15:16:11 -0800

    Added NavMeshAgentAnimatorController

---
+++ Assets/Scripts/AI/NavMeshAgentAnimatorController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshAgentAnimatorConnector : MonoBehaviour
+{
+    public NavMeshAgent navMeshAgent;
+    public Animator animator;
+    public string animatorSpeedVariable = "speed";
+
+    private void OnValidate()
+    {
+        navMeshAgent = GetComponentInChildren<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat(animatorSpeedVariable, navMeshAgent.velocity.magnitude);
+    }
+}
