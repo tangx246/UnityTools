40d4774292d651f238354df8e4edc5c6affa9752
Yee Chien Tang <tangx246@umn.edu>
2021-01-08 14:21:46 -0800

    Added minVelocity to NavMeshAgentAnimatorConnector

--- Assets/Scripts/AI/NavMeshAgentAnimatorConnector.cs
+++ Assets/Scripts/AI/NavMeshAgentAnimatorConnector.cs
@@ -1,22 +1,31 @@
 ﻿using UnityEngine;
 using UnityEngine.AI;
 
-public class NavMeshAgentAnimatorConnector : MonoBehaviour
+namespace UnityTools
 {
-    public NavMeshAgent navMeshAgent;
-    public Animator animator;
-    public string animatorSpeedVariable = "speed";
-    public float speedMultiplier = 1f;
-
-    private void OnValidate()
+    public class NavMeshAgentAnimatorConnector : MonoBehaviour
     {
-        navMeshAgent = GetComponentInChildren<NavMeshAgent>();
-        animator = GetComponentInChildren<Animator>();
-    }
+        public NavMeshAgent navMeshAgent;
+        public Animator animator;
+        public string animatorSpeedVariable = "speed";
+        public float speedMultiplier = 1f;
+        public float minVelocity = 0.1f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        animator.SetFloat(animatorSpeedVariable, navMeshAgent.velocity.magnitude * speedMultiplier);
+        private void OnValidate()
+        {
+            navMeshAgent = GetComponentInChildren<NavMeshAgent>();
+            animator = GetComponentInChildren<Animator>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            var agentVelocity = navMeshAgent.velocity.magnitude;
+            if (agentVelocity < minVelocity)
+            {
+                agentVelocity = 0;
+            }
+            animator.SetFloat(animatorSpeedVariable, agentVelocity * speedMultiplier);
+        }
     }
 }
