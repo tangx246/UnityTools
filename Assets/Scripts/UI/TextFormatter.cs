89bddad7903ca4de85bd4173656b6881c10b80ba
Yee Chien Tang <tangx246@umn.edu>
2021-05-18 15:32:14 -0700

    Used a bunch of overloads for int and floats instead of object, so that Unity Editor can see it

--- Assets/Scripts/UI/TextFormatter.cs
+++ Assets/Scripts/UI/TextFormatter.cs
@@ -15,12 +15,22 @@ namespace UnityTools
             textBox = GetComponent<TextMeshProUGUI>();
         }
 
-        public void SetText(object arg0)
+        public void SetText(int arg0)
         {
             textBox.SetText(string.Format(text, arg0));
         }
 
-        public void SetText(object _, object newValue)
+        public void SetText(int _, int newValue)
+        {
+            textBox.SetText(string.Format(text, newValue));
+        }
+
+        public void SetText(float arg0)
+        {
+            textBox.SetText(string.Format(text, arg0));
+        }
+
+        public void SetText(float _, float newValue)
         {
             textBox.SetText(string.Format(text, newValue));
         }
