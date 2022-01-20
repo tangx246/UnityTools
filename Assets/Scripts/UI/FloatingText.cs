4c1fd6eb56ca5713a4f1995cf9800464d4fdf2b2
Yee Chien Tang <tangx246@umn.edu>
2022-01-20 14:13:56 -0800

    - Added new border for progress bars - Made Floating Text use Mesh instead

--- Assets/Scripts/UI/FloatingText.cs
+++ Assets/Scripts/UI/FloatingText.cs
@@ -5,13 +5,10 @@ namespace UnityTools
 {
     public class FloatingText : MonoBehaviour
     {
-        public Text text;
         public Vector3 velocity = new Vector3(0, 0.1f, 0);
 
         public void Start()
         {
-            text = GetComponentInChildren<Text>();
-
             var camera = Camera.main;
             if (camera != null)
             {
