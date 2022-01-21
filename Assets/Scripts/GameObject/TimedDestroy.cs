b54c5093df7511bf82efbeffe7afa5a51ace43d2
Yee Chien Tang <tangx246@umn.edu>
2022-01-20 23:20:26 -0800

    Added GameObjectPool Release to Timed Destroy

--- Assets/Scripts/GameObject/TimedDestroy.cs
+++ Assets/Scripts/GameObject/TimedDestroy.cs
@@ -1,14 +1,23 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 namespace UnityTools
 {
     public class TimedDestroy : MonoBehaviour
     {
         public float maxLifeSeconds = 3f;
+        public bool releaseIntoGameObjectPooler = false;
 
-        void Start()
+        IEnumerator Start()
         {
-            Destroy(gameObject, maxLifeSeconds);
+            yield return new WaitForSeconds(maxLifeSeconds);
+            if (releaseIntoGameObjectPooler)
+            {
+                GameObjectPooler.instance.Release(gameObject);
+            } else
+            {
+                Destroy(gameObject);
+            }
         }
     }
 }
