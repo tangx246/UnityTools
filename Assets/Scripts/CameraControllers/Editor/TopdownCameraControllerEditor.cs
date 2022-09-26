1b7b8cc1125b4cd3cab9bd179eab43baa9ed60da
Yee Chien Tang <tangx246@umn.edu>
2022-09-26 16:08:20 -0700

    Added topdown camera controller

---
+++ Assets/Scripts/CameraControllers/Editor/TopdownCameraControllerEditor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(TopdownCameraController))]
+public class TopdownCameraControllerEditor : Editor
+{
+    public GameObject boundsTarget;
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        var cameraControls = (TopdownCameraController)target;
+
+        boundsTarget = (GameObject)EditorGUILayout.ObjectField("Bounds Target", boundsTarget, typeof(GameObject), true);
+        if (GUILayout.Button("Set Bounds"))
+        {
+            cameraControls.SetBounds(boundsTarget);
+        }
+    }
+}
---
+++ Assets/Scripts/CameraControllers/TopdownCameraController.cs
@@ -0,0 +1,95 @@
+using Cinemachine;
+using UnityEngine;
+using UnityTools;
+
+public class TopdownCameraController : MonoBehaviour
+{
+    public float scrollSpeed = 1f;
+    public float minZoom = 2.5f;
+    public float maxZoom = 7f;
+    public Bounds cameraBounds;
+    public GameObjectEventEmitter environmentFocusEvent;
+    public EventEmitter<Vector3> cameraCenterEvent;
+
+    private Vector2 moveDirection;
+    private new Camera camera;
+    private CinemachineVirtualCamera cinemachineVc;
+
+    private void Awake()
+    {
+        cinemachineVc = transform.parent.GetComponentInChildren<CinemachineVirtualCamera>();
+        cameraBounds.extents = Vector3.positiveInfinity;
+    }
+
+    private void OnEnable()
+    {
+        if (environmentFocusEvent != null)
+        {
+            environmentFocusEvent.eventEmitter.AddListener(SetBounds);
+        }
+
+        if (cameraCenterEvent != null)
+        {
+            cameraCenterEvent.eventEmitter.AddListener(SetPosition);
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (environmentFocusEvent != null)
+        {
+            environmentFocusEvent.eventEmitter.RemoveListener(SetBounds);
+        }
+
+        if (cameraCenterEvent != null)
+        {
+            cameraCenterEvent.eventEmitter.RemoveListener(SetPosition);
+        }
+    }
+
+    private void Update()
+    {
+        if (camera == null)
+        {
+            camera = Camera.main;
+        }
+
+        var cameraRelativeDirection = camera.transform.rotation * moveDirection;
+        var cameraProjection = Vector3.ProjectOnPlane(cameraRelativeDirection, Vector3.up).normalized;
+
+        SetPosition(transform.position + scrollSpeed * Time.unscaledDeltaTime * cameraProjection);
+    }
+
+    public void SetPosition(Vector3 pos)
+    {
+        transform.position = cameraBounds.ClosestPoint(pos);
+    }
+
+    public void SetBounds(GameObject parentWithRenderers)
+    {
+        Bounds bounds = new();
+        var renderers = parentWithRenderers.GetComponentsInChildren<Renderer>();
+        foreach (var renderer in renderers)
+        {
+            bounds.Encapsulate(renderer.bounds);
+        }
+
+        var terrains = parentWithRenderers.GetComponentsInChildren<Terrain>();
+        foreach (var terrain in terrains)
+        {
+            bounds.Encapsulate(terrain.terrainData.bounds);
+        }
+
+        cameraBounds = bounds;
+    }
+
+    public void Move(Vector2 direction)
+    {
+        moveDirection = direction.normalized;
+    }
+
+    public void Zoom(float value)
+    {
+        cinemachineVc.m_Lens.OrthographicSize = Mathf.Clamp(cinemachineVc.m_Lens.OrthographicSize - (value / 100), minZoom, maxZoom);
+    }
+}
