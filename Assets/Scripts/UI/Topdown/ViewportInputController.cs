4fc01b84ab1cc95cb88bca9745639b1a263d023d
Yee Chien Tang <tangx246@umn.edu>
2022-09-28 16:05:29 -0700

    - Created a ViewInputController - Moved DragSelector into a Topdown category

---
+++ Assets/Scripts/UI/Topdown/ViewportInputController.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class ViewportInputController : MonoBehaviour, IPointerClickHandler
+{
+    public ClickEvent selectEvent = new();
+    public ClickEvent actionEvent = new();
+
+
+    [Serializable]
+    public class ClickEvent : UnityEvent<Vector3> { } // Viewport clicked on screen position
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Left)
+        {
+            selectEvent.Invoke(eventData.pressPosition);
+        } else
+        {
+            actionEvent.Invoke(eventData.pressPosition);
+        }
+    }
+}
