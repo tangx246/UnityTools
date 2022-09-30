fdce633ae8bd96a12a94830e8fa824a6745f3cda
Yee Chien Tang <tangx246@umn.edu>
2022-09-30 11:18:16 -0700

    Add Registrar

---
+++ Assets/Scripts/GameObject/Registrar.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public static class Registrar<T> where T : Behaviour
+{
+    private readonly static HashSet<T> items = new();
+
+    private readonly static UnityEvent<T> addEvent = new();
+    private readonly static UnityEvent<T> removeEvent = new();
+
+    public static void Register(T item)
+    {
+        items.Add(item);
+        addEvent.Invoke(item);
+
+        var destroyHook = item.gameObject.AddComponent<OnDestroyHook>();
+        destroyHook.hook = () =>
+        {
+            items.Remove(item);
+            removeEvent.Invoke(item);
+        };
+    }
+
+    public static void AddRegisterListener(Behaviour listener, UnityAction<T> action)
+    {
+        AddListener(listener, action, addEvent);
+    }
+
+    private static void AddListener(Behaviour listener, UnityAction<T> action, UnityEvent<T> unityEvent)
+    {
+        unityEvent.AddListener(action);
+        var destroyHook = listener.gameObject.AddComponent<OnDestroyHook>();
+        destroyHook.hook = () => addEvent.RemoveListener(action);
+    }
+
+    public static void AddUnregisterListener(Behaviour listener, UnityAction<T> action)
+    {
+        AddListener(listener, action, removeEvent);
+    }
+
+    public static IEnumerable<T> GetAll()
+    {
+        return items;
+    }
+}
+
+public class OnDestroyHook : MonoBehaviour
+{
+    public Action hook;
+
+    private void OnDestroy()
+    {
+        hook();
+    }
+}
